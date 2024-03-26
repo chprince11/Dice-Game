1c174f8a7b07cc93a8d6e4fd5fe6fe477ad171bc
chprince11 <chprince@sheridancollege.com>
2024-03-26 04:36:39 -0400

    win_logic_implemented /Prince

--- index.js
+++ index.js
@@ -13,3 +13,16 @@
  var randomImageSource2 = "images/dice" + randomnumber2 + ".png";
 
  var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2 );
+
+ //If player 1 wins
+ if (randomnumber1 > randomnumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+ } 
+
+ else if(randomnumber2 > randomnumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+ }
+
+ else {
+    document.querySelector("h1").innerHTML = "Draw!"
+ }

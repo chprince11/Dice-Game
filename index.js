1731982ff3a8614b6041b8d9b0af96e95e620be6
chprince11 <chprince@sheridancollege.com>
2024-03-26 04:24:25 -0400

    image-logic-applied /Prince

--- index.js
+++ index.js
@@ -2,4 +2,14 @@
 
  var randomDiceImage = "dice" + randomnumber1 + ".png"; 
  
- document.querySelector("img").setAttribute("src", "")
+ var randomImageSource = "images/" + randomDiceImage;
+
+ var image1 = document.querySelectorAll("img")[0];
+
+ image1.setAttribute("src", randomImageSource );
+ 
+ var randomnumber2 = Math.floor( Math.random() * 6) + 1 ;
+
+ var randomImageSource2 = "images/dice" + randomnumber2 + ".png";
+
+ var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2 );
